16c465666c4fa43bd676c78190577849e1599d42
rafaelfmar <rafael.fmarques@outlook.com>
2019-11-07 20:53:58 -0300

    Ajustes em PautaController

--- sireata-control/src/main/java/br/edu/utfpr/dv/sireata/apresentacao/PautaController.java
+++ sireata-control/src/main/java/br/edu/utfpr/dv/sireata/apresentacao/PautaController.java
@@ -2,6 +2,8 @@ package br.edu.utfpr.dv.sireata.apresentacao;
 
 import br.edu.utfpr.dv.sireata.dao.PautaDAO;
 import br.edu.utfpr.dv.sireata.model.Pauta;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.sql.SQLException;
 import java.util.List;
 
+@Controller
+@RequiredArgsConstructor
 public class PautaController {
 
-    public PautaDAO pautaDAO;
+    private final PautaDAO pautaDAO;
 
-    @GetMapping("/ata/{ata}")
+    @GetMapping("/pauta/ata/{idAta}")
     public List<Pauta> listarPorAta(@PathVariable int idAta) throws SQLException {
         return pautaDAO.listarPorAta(idAta);
     }
